814ed0dfcd2876f5175cd4f54834c94944fa930d
Hale Pham <phamanhtu19297@gmail.com>
2021-05-10 00:45:22 +0700

    update for latest version
    Signed-off-by: Hale Pham <phamanhtu19297@gmail.com>

--- wl_defines.sh
+++ wl_defines.sh
@@ -1,6 +1,6 @@
 export INCLUDE_XWAYLAND=1
-export WLROOT=$HOME/Wayland
-export WLD=$WLROOT/install   # change this to another location if you prefer
+export WLROOT=/data/Wayland
+export WLD=/usr/local/   # change this to another location if you prefer
 
 if [ "$(uname -i)" = "i386" ]; then
 	export WL_BITS=32
@@ -8,12 +8,10 @@ else
 	export WL_BITS=64
 fi
 
-XWAYLAND=${WLROOT}/install/bin/Xwayland
+XWAYLAND=${WLD}/bin/Xwayland
 export DISTCHECK_CONFIGURE_FLAGS="--with-xserver-path=$XWAYLAND"
 
-export LD_LIBRARY_PATH=$WLD/lib
-export PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
-export PATH=$WLD/bin:$PATH
+export PATH=${HOME}/.local/bin:$PATH
 export ACLOCAL_PATH="$WLD/share/aclocal"
 export ACLOCAL="aclocal -I $ACLOCAL_PATH"
 
